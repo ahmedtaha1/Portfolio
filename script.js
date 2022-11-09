7fef5f79fc7d94e03f77555bb38e860732bc9f55
ahmedtaha1 <at0421801@gmail.com>
2022-11-09 19:10:06 +0200

    fi

--- script.js
+++
@@ -1,9 +0,0 @@
-let lis = document.querySelectorAll("li");
-lis.forEach((li) =>
-  li.addEventListener("click", () => {
-    li.parentElement
-      .querySelectorAll(".active")
-      .forEach((active) => active.classList.remove("active"));
-    li.classList.add("active");
-  })
-);
